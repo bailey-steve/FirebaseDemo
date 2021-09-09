924bdba2f3edf0aad78895acee4869b53f7de4a5
Steve Bailey <a@a.com>
2021-09-09 13:14:30 +0100

    Initial Commit

---
+++ Firebase Demo/Firebase_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_DemoApp.swift
+//  Firebase Demo
+//
+//  Created by user198386 on 09/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
